cf1529e119eeb4f7faf79ea38f2d12fba190699e
Tom Gibson <Tom.Gibson@optasense.com>
2016-09-14 13:42:05 +0100

    A simple java quiz.
    Run the batch file and answer the 10 questions as prompted.

---
+++ Quiz/dist/Quiz.bat
@@ -0,0 +1,4 @@
+@ECHO OFF
+SET JRE_HOME="C:\Program Files\Java\jre7"
+%JRE_HOME%\bin\java.exe -jar Quiz.jar
+pause
---
+++ Quiz/src/Quizmaker/Answer.java
@@ -0,0 +1,17 @@
+/*
+ * *(c) OptaSense Ltd 2016.  This software and the intellectual property in it is proprietary to OptaSense Ltd and/or its licensors./*
+ * *Your use of it is subject to the terms and restrictions set out in the contract under which it was supplied.
+ * *You must not use it for any other purpose without OptaSense's prior written permission and in particular, but without limitation,
+ * *you must not copy, reverse engineer or decompile this software nor permit or purport to permit any third party to do (other than and to the extent the same cannot be prohibited by law).
+ */
+package Quizmaker;
+
+/**
+ *
+ * @author Tom.Gibson
+ */
+public enum Answer {
+    CORRECT,INCORRECT;
+}
+
+
